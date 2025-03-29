9283198b9988d45b9713f405f6557e80962e17a9
JudithUruchima <JudithUruchima@users.noreply.github.com>
2025-03-29 11:51:24 -0500

    correccion de escritura

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ valor = os.environ.get("INPUT_VAL")
 
 #mostrar resultados en consola
 if "GITHUB_OUTPUT" in os.environ:
-    with open(os.environ["GI=ITHUB_OUTPUT"], "a") as f:
+    with open(os.environ["GITHUB_OUTPUT"], "a") as f:
         print("{0} = Hola{1}".format('result', valor), file=f);
